9b2ecbac6f09e73af8c313087cd97b40fc5cf705
Valentin BRAULT <valentin.brault2021@campus-eni.fr>
2021-10-12 15:56:08 +0200

    Fix accueil

--- .history/src/Controller/MainController_20211012151026.php
+++ .history/src/Controller/MainController_20211012151026.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use App\Form\MonProfilType;
-use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
