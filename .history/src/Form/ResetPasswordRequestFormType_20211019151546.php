c759545d9b69b9771fdd404d0beea4a0181643b6
Valentin BRAULT <valentin.brault2021@campus-eni.fr>
2021-10-19 15:52:02 +0200

    ajout de mot de passe oublié

---
+++ .history/src/Form/ResetPasswordRequestFormType_20211019151546.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ResetPasswordRequestFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                
+                'attr' => ['autocomplete' => 'email', 'placeholder' => 'Entrez votre email et on vous enverra un lien pour obtenir un nouveau mot de passe..'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Rentre ton email frerot',
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
